0c58656c34a4f034b6fe14c41fb68caaf07dedef
Yuriko Takamiya <yurikotakamiya@yurikonombp-2.home>
2021-12-08 19:38:43 -0500

    Changed App.js a bit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React from "react";
 import "./App.css";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [topic, setTopic] = useState([]);
+  const [currentTopic, setCurrentTopic] = useState(null);
+  const openTopic = id => {
+    setCurrentTopic(id);
+  }
+  const closeTopic = () => {
+    setCurrentTopic(null);
+  }
+  useEffect(() => {
+    const fetchPhotos = () => {
+      axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+      .then(res => {
+        console.log("Loaded!")
+        console.log(res.data)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+    }
+    fetchPhotos()
+  },[])
+
   return (
     <div className="App">
+      <h1>This is Nasa</h1>
+      
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
